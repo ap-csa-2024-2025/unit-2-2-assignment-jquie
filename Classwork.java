95818c9c4ab730f5f93e1f8db432706dbdc0b191
jquie <jacquelynphan03@gmail.com>
2024-10-28 10:17:53 -0700

    not done

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Scanner userInput = new Scanner(System.in);
+    System.out.println ("What type of item are you buying?");
+    String x = userInput.nextLine();
+    System.out.println(x);
+    System.out.println("How many are you buying?");
+    Integer y = userInput.nextInt();
+    System.out.println(y);
+    System.out.println ("How much does one weigh?");
+    Double z = userInput.nextDouble();
+    System.out.println(z);
+    System.out.println(y + " " + x + " at " + z + " pounds each will weigh " + (y*z) + " pounds total");
+
+
+    
+
   }
 }
