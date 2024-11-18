7bf2dd357117f3a760515657c06303d848f322c9
jquie <jacquelynphan03@gmail.com>
2024-11-18 17:46:52 +0000

    done!

--- Classwork.java
+++ Classwork.java
@@ -4,6 +4,7 @@ public class Classwork
 {
   public static void main(String[] args)
   {
+    //Shopping
     Scanner userInput = new Scanner(System.in);
     System.out.println ("What type of item are you buying?");
     String x = userInput.nextLine();
@@ -16,6 +17,11 @@ public class Classwork
     System.out.println(z);
     System.out.println(y + " " + x + " at " + z + " pounds each will weigh " + (y*z) + " pounds total");
 
+    //Quote
+    System.out.println("\"That brain of mine is something more than merely \n mortal; as time will show.\" \n Ada Lovelace \n The first computer programmer");
+
+    //Rabbit
+    System.out.println("(\\(\\ \n( - -) \n((') (')");
 
     
 
